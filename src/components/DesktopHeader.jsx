5caec36022c36200e99e031278fd05092e18ac34
ahmed025566 <ahmedsalah025566#gmail.com>
2024-01-17 20:39:09 +0200

    Make the desktop header sticky

--- src/components/DesktopHeader.jsx
+++ src/components/DesktopHeader.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import file from '../assets/Ahmed Salah.pdf';
 
 
 const DesktopHeader = () => {
+
+  const [background, setBackground] = useState('rgba(0, 0, 0, .00)'); 
+  useEffect(() => {
+    const handleScroll = () => {
+      const newScroll = window.scrollY;
+      let alpha =  newScroll <= 60 ? `.${newScroll}` : '.60'
+      setBackground(`rgba(0, 0, 0, ${alpha})`);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+ 
+
   return (
-    <header className="header" data-aos="slide-down">
+    <header className="header" style={{background:  background}} data-aos="slide-down">
     <NavLink to="/"><h1 className="logo">Ahmed</h1></NavLink>
     <nav className="navBar">
       <NavLink to="/" className="header-link">Home</NavLink>
